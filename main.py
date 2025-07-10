04fa6d8ccc118dd211bf5f6ea5d27638cde97b3f
watchmypizza <kevinosswald@proton.me>
2025-07-10 23:19:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from typing import Optional
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print("Synced " + str(len(synced)) + " commands.")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="test")
+@app_commands.describe(test="test")
+async def test(interaction: discord.Interaction, test: Optional[str]):
+    if not test:
+        await interaction.response.send_message("Hello!", ephemeral=True)
+        return
+    await interaction.response.send_message("Hello, this is super awesome {}".format(test), ephemeral=True)
+
+@bot.event
+async def on_member_join(member: discord.Member):
+    role = 1392625209906954372
+    x = member.guild.get_role(role)
+    member.add_roles(role)
+    embed = discord.Embed(
+        title=f"Welcome {member.display_name}!",
+        description="It's nice to see you here! We hope you'll enjoy your stay!!",
+        color=discord.Color.random()
+    )
+
+    if member.avatar is None:
+        avatar_url = member.default_avatar.url
+    else:
+        avatar_url = member.avatar.url
+
+    embed.set_thumbnail(url=avatar_url)
+
+    channel = member.guild.system_channel
+    if channel:
+        await channel.send(embed=embed)
+
+@bot.tree.command(name="warn", description="Give someone a warning for a rule break.")
+@app_commands.describe(
+    member="The member to warn.",
+    reason="Why the warning is issued.",
+    proof="Link to the image or screenshot proof (upload it somewhere and paste link)."
+)
+async def warn(interaction: discord.Interaction, member: discord.Member, reason: str, proof: str):
+    channel = 1392974737101029456
+    x = interaction.guild.get_channel(channel)
+    if not proof.startswith("http"):
+        await interaction.response.send_message("❌ Please provide a valid link to the proof.", ephemeral=True)
+        return
+
+    await interaction.response.send_message("The user has been warned.", ephemeral=True)
+
+    await x.send(
+        f"⚠️ {member.mention} has been warned for: **{reason}**\n📎 Proof: {proof}"
+    )
+
+
+bot.run(TOKEN)
